c124c9562bee473e9a24ab06cae6bb43e991a273
surendranath.j <surendranath.j@wlpt1564.local>
2024-11-29 23:10:33 +0530

    change in linkedin extraction

--- utils.py
+++ utils.py
@@ -15,7 +15,7 @@ def get_linkedin_education(linkedin_education):
         education = ""
         if len(linkedin_education) > 0:
             latest_education = linkedin_education[0]
-            education = f"{latest_education.get("degree")} in {latest_education.get("fieldOfStudy")} from {latest_education.get("schoolName")}"
+            education = f"{latest_education.get('degree')} in {latest_education.get('fieldOfStudy')} from {latest_education.get('schoolName')}"
         return education.strip()
     except Exception as e:
         return ""
@@ -25,8 +25,8 @@ def get_linkedin_positions(linkedin_positions):
         positions = ""
         if len(linkedin_positions) > 0:
             for position in linkedin_positions:
-                position_text = f"{position.get("title")} at {position.get("companyName")}, {position.get("location")} from {position.get("start").get("year")}"
-                position_end_year = position.get("end").get("year")
+                position_text = f"{position.get('title')} at {position.get('companyName')}, {position.get('location')} from {position.get('start').get('year')}"
+                position_end_year = position.get('end').get('year')
                 if position_end_year > 0:
                     position_text = position_text + f" to {position_end_year} | "
                 else :
@@ -43,7 +43,7 @@ def get_linkedin_skills(linkedin_skills):
         skills = ""
         if len(linkedin_skills) > 0:
             for skill in linkedin_skills:
-                skills = skills + skill.get("name", "") + " | "
+                skills = skills + skill.get('name', '') + " | "
 
         return skills.strip()
     except Exception as e:
@@ -54,7 +54,7 @@ def get_linkedin_courses(linkedin_courses):
         courses = ""
         if len(linkedin_courses) > 0:
             for course in linkedin_courses:
-                courses = courses + course.get("name", "") + " | "
+                courses = courses + course.get('name', '') + " | "
 
         return courses.strip()
     except Exception as e:
@@ -65,7 +65,7 @@ def get_linkedin_certifications(linkedin_certifications):
         certifications = ""
         if len(linkedin_certifications) > 0:
             for certification in linkedin_certifications:
-                certifications = certifications + f"{certification.get("name", "")} from {certification.get("authority", "")}" + " | "
+                certifications = certifications + f"{certification.get('name', '')} from {certification.get('authority', '')} | "
 
         return certifications.strip()
     except Exception as e:
@@ -76,7 +76,7 @@ def get_linkedin_honors(linkedin_honors):
         honors = ""
         if len(linkedin_honors) > 0:
             for honor in linkedin_honors:
-                honors = honors + honor.get("title", "") + f" in {honor.get("issuedOn").get("year")} | "
+                honors = honors + honor.get('title', '') + f" in {honor.get('issuedOn').get('year')} | "
 
         return honors.strip()
     except Exception as e:
